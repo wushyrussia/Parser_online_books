7f4cbc3562705e8a5f484fb4f9a76ab0bea7ba2d
wushyrussia <denis.sedikov@yandex.ru>
2018-07-11 22:55:29 +0300

    /

--- src/Pfunct/Parser.java
+++ src/Pfunct/Parser.java
@@ -1,7 +1,11 @@
 package Pfunct;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import java.util.Scanner;
+
+//import java.io.*;
+
 
 public class Parser {
     public static void Cparser() throws Exception {
@@ -15,12 +19,12 @@ public class Parser {
                 .userAgent("Mozilla")
                 .post();
         System.out.println(doc);
-        lineCounter(doc);
+        M_LineCounter(doc);
 
     }
    //find count from html document
-       private static void lineCounter(Document doc) throws Exception {
-
+       private static void M_LineCounter(Document doc) throws Exception {
+        //get the number of lines
         int lineCount = 0, commentsCount = 0;
 
         Scanner input = new Scanner(String.valueOf(doc));
@@ -32,8 +36,40 @@ public class Parser {
             lineCount++;
         }
 
-     //   lineCount = lineCount-31;
         System.out.println("Line Count: " + lineCount + "\t Comments Count: " + commentsCount);
-    }
 
+        //   M_Writer(doc,lineCount);
+
+     //      M_NumberPage(lineCount,doc);
+    }
+  /*  private static void M_NumberPage(int line, Document doco) throws Exception {
+        line = line-31; //get the number string with number page
+        BufferedReader br = new BufferedReader(doco);
+        File f = new File("/root/2/Files.txt");
+        BufferedReader fin = new BufferedReader(new FileReader(doco));
+        String name;
+        String lines;
+        System.out.println("Print File "+f.getName()+"? y/n");
+        name = br.readLine();
+        if(name.equals("y"))
+            while ((lines = fin.readLine()) != null) System.out.println(lines);
+    }*/
+   /* public static void M_Writer(Document doc, int lineNumber) throws IOException {
+
+        try (FileWriter writer = new FileWriter("bufer", false)) {
+            // запись всей строки
+            writer.write(String.valueOf(doc));
+            // запись по символам
+
+            writer.flush();
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    public void  M_line(){
+
+
+        }*/
 }
