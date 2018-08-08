74937787b744b376ec2abd75a8de4041a87973b3
Denis.sedikov@yandex.ru <Denis.sedikov@yandex.ru>
2018-08-08 16:54:29 +0300

    Class BookA for android

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@
 import static pFunction.Parser.*;
 import static pFunction.Book.createBookMain;
 import pFunction.Book;
+import pFunction.BookA;
 
 
 
 public class Main{
 
     public static void main(String [] args) throws Exception {
-       Book TxtBook = new Book();
+       BookA TxtBook = new BookA();
+		// Book TxtBook = new Book();
 	   TxtBook.createBookMain();
 		//M_clear();
      //   M_Add_pg_to_File();
